6ce169c1b779f846ba077566c4685c56dd1ccdb5
FloppyTurtle <adh542@york.ac.uk>
2023-10-02 22:29:47 +0100

    Creating videos

--- Code/api-adapt.py
+++ Code/api-adapt.py
@@ -1,26 +1,49 @@
-class fbpic:
-    import h5py, os
-    from openpmd_viewer import OpenPMDTimeSeries
-    import matplotlib.pyplot as plt
-    import openpmd_api as io
+import h5py, os, platform
+from openpmd_viewer import OpenPMDTimeSeries
+import matplotlib.pyplot as plt
+import openpmd_api as io
+import subprocess
 
-    
-    
-    def __init__(self, relativeInputPath = "\\diags\\hdf5"):
+class fbpic:
+    def __init__(self, relativeInputPath = ""):
+        if relativeInputPath == "":
+            if platform.system() == "Linux":
+                relativeInputPath = "/diags/hdf5"
+            elif platform.system() == "Windows":
+                relativeInputPath = "\\diags\\hdf5"
+
+
         self.cwd = os.getcwd()
         self.inputDataPath =  self.cwd + relativeInputPath
         self.relativeInputPath = relativeInputPath
 
-        self.ts = OpenPMDTimeSeries(inputDataPath)
+        self.ts = OpenPMDTimeSeries(self.inputDataPath)
         self.it = self.ts.iterations
-        print(it)
+        print(self.it)
 
     def listFields(self):
-        self.ts.avail_fields
+        print(self.ts.avail_fields)
 
-        
+    def slider(self):
+        self.ts.slider()
+        plt.show()
 
-for iter in it: 
+    def saveFigures(self,outDir="results",iterations = [],fields=[],particles=[]):
+        if not(os.path.exists(self.cwd+"/"+outDir)):
+            subprocess.run(["mkdir", outDir])
+
+        for iter in self.it:
+            for field_iter in fields:
+                plt.clf()
+                plt.gcf()
+                var, var_info = self.ts.get_field(iteration=iter,field =  field_iter,slice_across=["z"])
+
+
+series = fbpic()
+series.listFields()
+#series.slider()
+
+""" for iter in it: 
     rho, info_rho = ts.get_field( iteration=iter, field='rho',plot=True )
     plt.show()
-    plt.close()
+    plt.close() """
