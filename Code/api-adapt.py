3e01eec8fda27d0c9db5a77c16a256e78ce88246
FloppyTurtle <adh542@york.ac.uk>
2023-10-03 13:03:43 +0100

    Saving figures

--- Code/api-adapt.py
+++ Code/api-adapt.py
@@ -6,13 +6,15 @@ import subprocess
 
 class fbpic:
     def __init__(self, relativeInputPath = ""):
-        if relativeInputPath == "":
-            if platform.system() == "Linux":
+        ## Find what platform we are on necessary for saving files
+        self.platform = platform.system()
+        if not relativeInputPath:
+            if self.platform == "Linux":
                 relativeInputPath = "/diags/hdf5"
-            elif platform.system() == "Windows":
+            elif self.platform == "Windows":
                 relativeInputPath = "\\diags\\hdf5"
 
-
+        ## Get the data input path and set it
         self.cwd = os.getcwd()
         self.inputDataPath =  self.cwd + relativeInputPath
         self.relativeInputPath = relativeInputPath
@@ -21,23 +23,73 @@ class fbpic:
         self.it = self.ts.iterations
         print(self.it)
 
+    ## List all avaliable fields
     def listFields(self):
         print(self.ts.avail_fields)
 
+    ## This only seems to work in Juypter and not on my linux setup
     def slider(self):
         self.ts.slider()
         plt.show()
 
-    def saveFigures(self,outDir="results",iterations = [],fields=[],particles=[]):
+    def saveFigures(self, outDir:"CWD/outdir"="results",
+                     iterations:"SPecific iteratations" = [],
+                     fields:"Specific fields"=[],
+                     particles:"Specific particles"=[]):
+        
+        ## Create the result directory
         if not(os.path.exists(self.cwd+"/"+outDir)):
             subprocess.run(["mkdir", outDir])
 
-        for iter in self.it:
-            for field_iter in fields:
-                plt.clf()
-                plt.gcf()
-                var, var_info = self.ts.get_field(iteration=iter,field =  field_iter,slice_across=["z"])
+        ## Assign the lists to the ones avaliable
+        if not iterations:
+            iterations = self.it
 
+        if not fields:
+            fields = self.ts.avail_fields
+
+        if not particles:
+            particles = self.ts.avail_species
+
+
+        ## Same function but one for linux and one for windows since the address
+        ## method is different for each operating system
+        if self.platform == "Linux" or self.platform == "linux":
+            for field_iter in fields:
+                fileList = []
+                for iter in iterations: 
+                    plt.clf()
+                    plt.gcf()
+                    var, var_info = self.ts.get_field(iteration=iter,field =  field_iter,slice_across=["z"])
+                    plt.colorbar()
+                    fileList.append(self.liSave(outDir,iter,field_iter,dpi=300))
+
+        elif self.platform == "Windows" or self.platform == "windows":
+            for field_iter in fields:
+                fileList = []
+                for iter in iterations: 
+                    plt.clf()
+                    plt.gcf()
+                    var, var_info = self.ts.get_field(iteration=iter,field =  field_iter,slice_across=["z"])
+                    plt.colorbar()
+                    fileList.append(self.liSave(outDir,iter,field_iter,dpi=300))
+
+                    
+
+    def itg(self,fps = 1):
+        pass
+
+    ## Linux save function using the correct file notation
+    def liSave(self,outDir,iter,field_iter,dpi):
+        path = "./"+outDir+"/iter:"+iter+":"+field_iter+".png"
+        plt.savefig(path, dpi=300)
+        return path
+    
+    ## Windows save notation using the correct notation
+    def winSave(self,outDir,iter,field_iter,dpi):
+        path = self.cwd+"\\"+outDir+"\\iter:"+iter+":"+field_iter+".png"
+        plt.savefig(path, dpi=300)
+        return path
 
 series = fbpic()
 series.listFields()
